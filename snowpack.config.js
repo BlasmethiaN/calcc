46da7fc502113a36585a9f7f9cbeb956fdfab965
BlasmethiaN <dominako223@gmail.com>
2021-03-09 17:30:10 +0100

    initial commit

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,10 +1,13 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    /* ... */
+    public: {url: '/', static: true},
+    src: {url: '/dist'},
   },
   plugins: [
-    /* ... */
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
